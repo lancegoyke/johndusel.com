2ddf0ed0fcf28dfa1b81f2b98ec6f2183fb6a83b
Lance Goyke <lancegoyke@gmail.com>
2023-02-12 13:00:35 -0700

    get more user information

--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from .models import Post
+from users.serializers import UserSerializer
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
     class Meta:
         model = Post
         fields = "__all__"
