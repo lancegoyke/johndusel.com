f22696c1fd1475f3a589d582e222d129e7e3da17
Lance Goyke <lancegoyke@gmail.com>
2023-02-15 07:49:13 -0700

    order list of posts by newest to oldest

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -5,5 +5,5 @@ from .serializers import PostSerializer
 
 
 class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
