ea123b44105c0cd2db94e5170a940fb697a0e442
Lance Goyke <lancegoyke@gmail.com>
2023-02-15 07:31:58 -0700

    add post categories

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -9,6 +9,17 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField('Category', related_name='posts', blank=True)
 
     def __str__(self) -> str:
         return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
--- backend/blog/serializers.py
+++ backend/blog/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Category
 from users.serializers import UserSerializer
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["name", "slug"]
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer()
+    categories = CategorySerializer(many=True)
 
     class Meta:
         model = Post
