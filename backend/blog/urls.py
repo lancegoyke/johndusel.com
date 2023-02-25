bb2be90dfd8526f328c78b731a80534aa10355e2
Lance Goyke <lance@lancegoyke.com>
2023-02-25 12:25:19 -0700

    API endpoint for filtering posts by category

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from .views import PostListAPIView, PostListLatestAPIView, PostDetailAPIView
+from .views import (
+    PostListAPIView,
+    PostListByCategoryAPIView,
+    PostListLatestAPIView,
+    PostDetailAPIView,
+)
 
 
 urlpatterns = [
-    path('posts/latest/', PostListLatestAPIView.as_view(), name='posts_latest'),
-    path('posts/<str:slug>/', PostDetailAPIView.as_view(), name='post_detail'),
-    path('posts/', PostListAPIView.as_view(), name='posts'),
+    path(
+        "posts/category/<str:category_slug>/",
+        PostListByCategoryAPIView.as_view(),
+        name="posts_by_category",
+    ),
+    path("posts/latest/", PostListLatestAPIView.as_view(), name="posts_latest"),
+    path("posts/<str:slug>/", PostDetailAPIView.as_view(), name="post_detail"),
+    path("posts/", PostListAPIView.as_view(), name="posts"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -20,6 +20,18 @@ class PostListAPIView(generics.ListAPIView):
     serializer_class = PostSerializer
 
 
+class PostListByCategoryAPIView(generics.ListAPIView):
+    queryset = Post.objects\
+        .select_related("author")\
+        .prefetch_related("categories")\
+        .order_by("-created_at")
+    serializer_class = PostSerializer
+
+    def get_queryset(self):
+        category_slug = self.kwargs["category_slug"]
+        return self.queryset.filter(categories__slug=category_slug)
+
+
 class PostDetailAPIView(generics.RetrieveAPIView):
     queryset = Post.objects.select_related("author")
     serializer_class = PostSerializer
