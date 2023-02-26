6c96a4c4d16dee72f87e2757cf678fac0860a2ef
Lance Goyke <lance@lancegoyke.com>
2023-02-26 15:55:31 -0700

    change admin URL path

--- backend/cms/urls.py
+++ backend/cms/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('backside/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('api/', include('blog.urls')),
 ]
