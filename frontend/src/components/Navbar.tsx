17a51818e39df268d9c72fb3797f1ea3c7f5004d
Lance Goyke <lance@lancegoyke.com>
2023-03-08 08:11:25 -0700

    conver <a> to <Link>

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,15 @@
-import styles from '../styles/Navbar.module.css'
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
   return (
     <header className={styles.menu}>
       <div className={styles.logo}>Admin User</div>
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="#">Contact</a>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="#">Contact</Link>
       </nav>
     </header>
-  )
+  );
 }
