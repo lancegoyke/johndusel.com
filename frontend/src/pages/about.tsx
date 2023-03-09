f8c114c70e7cfa65dc97bafdf7ecd5bcf1f4a435
Lance Goyke <lance@lancegoyke.com>
2023-03-09 13:32:15 -0700

    add note about conference

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -20,48 +21,66 @@ export default function About() {
           <div className="center">
             <div className="stack-recursive">
               <h1>About</h1>
-              <p>
-                I am an Athletic Trainer and Strength & Conditioning Coach for
-                the Atlanta Hawks.
-              </p>
-              <p>
-                I have experience working along the entire rehabilitation to
-                performance spectrum. I pride myself in being a good teammate
-                and consistently working to improve at my craft.
-              </p>
-              <h2>Experience</h2>
-              <ul>
-                <li>
-                  Atlanta Hawks – Athletic Trainer and Strength & Conditioning
-                  Coach
-                </li>
-                <li>
-                  Golden State Warriors – Athletic Trainer and Strength &
-                  Conditioning Coach
-                </li>
-                <li>
-                  Cressey Sports Performance – Strength & Conditioning Intern
-                </li>
-              </ul>
-              <h2>Lecturing</h2>
-              <ul>
-                <li>
-                  Presented staff in-services on return-to-play protocols and
-                  biomechanics of landing
-                </li>
-              </ul>
-              <h2>Education</h2>
-              <ul>
-                <li>Certified Athletic Trainer</li>
-                <li>
-                  Master&apos;s Degree in Sport Science and Coach Education from
-                  East Tennessee State University
-                </li>
-                <li>
-                  Bachelor&apos;s Degree in Athletic Training from University of
-                  New England
-                </li>
-              </ul>
+              <section>
+                <p>
+                  I am an Athletic Trainer and Strength & Conditioning Coach for
+                  the Atlanta Hawks.
+                </p>
+                <p>
+                  I have experience working along the entire rehabilitation to
+                  performance spectrum. I pride myself in being a good teammate
+                  and consistently working to improve at my craft.
+                </p>
+              </section>
+
+              <section>
+                <h2>Experience</h2>
+                <ul>
+                  <li>
+                    Atlanta Hawks – Athletic Trainer and Strength & Conditioning
+                    Coach
+                  </li>
+                  <li>
+                    Golden State Warriors – Athletic Trainer and Strength &
+                    Conditioning Coach
+                  </li>
+                  <li>
+                    Cressey Sports Performance – Strength & Conditioning Intern
+                  </li>
+                </ul>
+              </section>
+
+              <section>
+                <h2>Lecturing</h2>
+                <ul>
+                  <li>
+                    <em>Upcoming:</em> I'll be speaking at the{" "}
+                    <Link href="https://elitebasketballrehabconference.com/">
+                      Elite Basketball Rehab Conference
+                    </Link>{" "}
+                    in July 2023
+                  </li>
+                  <li>
+                    Presented staff in-services on return-to-play protocols and
+                    biomechanics of landing
+                  </li>
+                </ul>
+              </section>
+
+              <section>
+                <h2>Education</h2>
+                <ul>
+                  <li>Certified Athletic Trainer</li>
+                  <li>
+                    Master&apos;s Degree in Sport Science and Coach Education
+                    from East Tennessee State University
+                  </li>
+                  <li>
+                    Bachelor&apos;s Degree in Athletic Training from University
+                    of New England
+                  </li>
+                </ul>
+              </section>
             </div>
           </div>
         </div>
