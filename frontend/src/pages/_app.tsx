85b6e4084eedaca4ff240d0748b721faf4ae3ac7
Lance Goyke <lance@lancegoyke.com>
2023-03-09 13:25:48 -0700

    update fonts

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Roboto_Flex } from "@next/font/google";
+
+const mainFont = Roboto_Flex({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${mainFont.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
