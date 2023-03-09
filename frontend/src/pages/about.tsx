4661eb41e4126aafd70b21f32e50f5bd14b00e3b
Lance Goyke <lance@lancegoyke.com>
2023-03-09 12:34:33 -0700

    layout info for About page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,13 +1,16 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 export default function About() {
   return (
     <>
       <Head>
         <title>About</title>
-        <meta name="description" content="Professional work in an impressive field" />
+        <meta
+          name="description"
+          content="I am an Athletic Trainer and Strength & Conditioning Coach for the Atlanta Hawks"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -18,16 +21,52 @@ export default function About() {
             <div className="stack-recursive">
               <h1>About</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                I am an Athletic Trainer and Strength & Conditioning Coach for
+                the Atlanta Hawks.
               </p>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                I have experience working along the entire rehabilitation to
+                performance spectrum. I pride myself in being a good teammate
+                and consistently working to improve at my craft.
               </p>
+              <h2>Experience</h2>
+              <ul>
+                <li>
+                  Atlanta Hawks – Athletic Trainer and Strength & Conditioning
+                  Coach
+                </li>
+                <li>
+                  Golden State Warriors – Athletic Trainer and Strength &
+                  Conditioning Coach
+                </li>
+                <li>
+                  Cressey Sports Performance – Strength & Conditioning Intern
+                </li>
+              </ul>
+              <h2>Lecturing</h2>
+              <ul>
+                <li>
+                  Presented staff in-services on return-to-play protocols and
+                  biomechanics of landing
+                </li>
+              </ul>
+              <h2>Education</h2>
+              <ul>
+                <li>Certified Athletic Trainer</li>
+                <li>
+                  Master's Degree in Sport Science and Coach Education from East
+                  Tennessee State University
+                </li>
+                <li>
+                  Bachelor's Degree in Athletic Training from University of New
+                  England
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </main>
       <Footer />
     </>
-  )
+  );
 }
