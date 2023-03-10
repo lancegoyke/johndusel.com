9e727aff2b06f57d1a5fcc9c1a9c365715aa0a3d
Lance Goyke <lance@lancegoyke.com>
2023-03-10 09:45:43 -0700

    allow more time for site builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 1000,
   env: {
     SITE_TITLE: "John Dusel",
     SITE_DESCRIPTION:
