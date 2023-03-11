bbc84ce6924732cb142c747248a98c131110787c
Lance Goyke <lance@lancegoyke.com>
2023-03-11 12:00:03 -0700

    update About page

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main>
+      <main id="about">
         <div>
           <div className="center">
             <div className="stack-recursive">
@@ -31,26 +31,40 @@ export default function About() {
                   performance spectrum. I pride myself in being a good teammate
                   and consistently working to improve at my craft.
                 </p>
+                <p>
+                  I was fortunate enought to be a part of the Golden State
+                  Warriors{" "}
+                  <Link href="https://twitter.com/ATLHawks/status/1062539016992579584/photo/1">
+                    championship team
+                  </Link>
+                  .
+                </p>
               </section>
 
-              <section>
+              <section id="experience">
                 <h2>Experience</h2>
-                <ul>
-                  <li>
-                    Atlanta Hawks – Athletic Trainer and Strength & Conditioning
+                <article>
+                  <h3>Atlanta Hawks</h3>
+                  <p>
+                    2018 - Present: Athletic Trainer and Strength & Conditioning
                     Coach
-                  </li>
-                  <li>
-                    Golden State Warriors – Athletic Trainer and Strength &
-                    Conditioning Coach
-                  </li>
-                  <li>
-                    Cressey Sports Performance – Strength & Conditioning Intern
-                  </li>
-                </ul>
+                  </p>
+                </article>
+                <article>
+                  <h3>Golden State Warriors</h3>
+                  <p>
+                    2017 - 2018: Athletic Trainer and Strength & Conditioning
+                    Coach
+                  </p>
+                  <p>2016 - 2017: Performance Intern</p>
+                </article>
+                <article>
+                  <h3>Cressey Sports Performance</h3>
+                  <p>2016: Strength & Conditioning Intern</p>
+                </article>
               </section>
 
-              <section>
+              <section id="lecturing">
                 <h2>Lecturing</h2>
                 <ul>
                   <li>
@@ -67,7 +81,7 @@ export default function About() {
                 </ul>
               </section>
 
-              <section>
+              <section id="education">
                 <h2>Education</h2>
                 <ul>
                   <li>Certified Athletic Trainer</li>
