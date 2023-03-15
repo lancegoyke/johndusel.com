223d6411d4fb6e52d7274f9edd723da4bc8db29d
Lance Goyke <lance@lancegoyke.com>
2023-03-15 11:34:33 -0700

    functioning mailerlite newsletter signup form

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Script from "next/script";
 import pic from "../../public/john-dusel-clapping-on-court-in-red_2000x3000.jpg";
 import styles from "../styles/Hero.module.css";
 
 export default function Hero() {
+  const [formShown, setFormShown] = useState(true);
+  const [successMsgShown, setSuccessMsgShown] = useState(false);
+  const [failureMsgShown, setFailureMsgShown] = useState(false);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form as HTMLFormElement);
+
+    const subscribeUrl =
+      "https://assets.mailerlite.com/jsonp/363816/forms/82655463782483848/subscribe";
+
+    fetch(subscribeUrl, {
+      method: "POST",
+      body: formData,
+    }).then((response) => {
+      if (response.ok) {
+        setFormShown(false);
+        setSuccessMsgShown(true);
+      } else {
+        setFailureMsgShown(true);
+      }
+    });
+  }
+
   return (
     <div className={styles.heroContainer}>
       <div className={styles.hero}>
@@ -28,11 +55,67 @@ export default function Hero() {
             <p className={styles.cta}>
               Sign up to get notified when a new post comes out:
             </p>
-            <form>
-              <input type="email" placeholder="you@example.com" required />
-              <button type="submit">Subscribe</button>
+            <form
+              action=""
+              method="post"
+              target="_blank"
+              className={styles.subscribeForm}
+              data-code=""
+              onSubmit={handleSubmit}
+              style={{ display: formShown ? "block" : "none" }}
+            >
+              <div className="hidden">
+                <input type="hidden" name="ml-submit" value="1" />
+                <input type="hidden" name="anticsrf" value="true" />
+              </div>
+              <div className={styles.shown}>
+                <input
+                  aria-label="email"
+                  aria-required="true"
+                  type="email"
+                  name="fields[email]"
+                  placeholder="you@example.com"
+                  autoComplete="email"
+                  data-mask=""
+                  required
+                />
+                <button type="submit">Subscribe</button>
+              </div>
+              <button
+                disabled={true}
+                style={{ display: "none" }}
+                type="button"
+                className="loading"
+              >
+                <div className="ml-form-embedSubmitLoad"></div>
+                <span className="sr-only">Loading...</span>
+              </button>
             </form>
+
+            <div
+              className="successMsg alert success"
+              style={{ display: successMsgShown ? "block" : "none" }}
+            >
+              <div>
+                <p>
+                  <strong>Thank you!</strong> You have successfully joined our
+                  subscriber list.
+                </p>
+              </div>
+            </div>
+
+            <div
+              className="failureMsg alert error"
+              style={{ display: failureMsgShown ? "block" : "none" }}
+            >
+              <div>
+                <p>
+                  <strong>Uh oh!</strong> Something went wrong.
+                </p>
+              </div>
+            </div>
           </div>
+
           <div className="alert warning">
             <p>
               <em>Upcoming:</em> I&apos;ll be speaking at the{" "}
